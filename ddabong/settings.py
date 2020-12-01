133879bcaefe41963a6b8f2bfffcbd5f139f60ab
youn dong yeol <eastheat10@gmail.com>
2020-12-01 16:30:15 +0900

    heroku1

--- ddabong/settings.py
+++ ddabong/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
